3199f2e4a34dc3fbfd515bded689aacd3ff9d828
julioceno <julioreplay@gmail.com>
2025-11-30 18:45:14 -0300

    feat: initial commit

---
+++ src/main/java/com/julioceno/qrcodeservice/infrastructure/controllers/PingController.java
@@ -0,0 +1,16 @@
+package com.julioceno.qrcodeservice.infrastructure.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/ping")
+public class PingController {
+
+    @GetMapping
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
+    }
+}
---
+++ src/test/java/com/julioceno/qrcodeservice/QrcodeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julioceno.qrcodeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
