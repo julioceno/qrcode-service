1a21959ff4ef53b41180b643947ea6cf1357f64b
julioceno <julioreplay@gmail.com>
2025-12-03 20:54:20 -0300

    test: configure test profile and MongoDB for QrcodeserviceApplicationTests

--- src/test/java/com/julioceno/qrcodeservice/QrcodeserviceApplicationTests.java
+++ src/test/java/com/julioceno/qrcodeservice/QrcodeserviceApplicationTests.java
@@ -1,9 +1,13 @@
 package com.julioceno.qrcodeservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.data.mongodb.test.autoconfigure.AutoConfigureDataMongo;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureDataMongo
 class QrcodeserviceApplicationTests {
 
 	@Test
